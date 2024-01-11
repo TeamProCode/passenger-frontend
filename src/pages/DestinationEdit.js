a60a4eef2220026355ac54f95d11466a71f7ec9f
LEARN Academy <learnacademy@MacBook-Air.local>
2024-01-11 15:01:20 -0800

    partial edit functionality

--- src/pages/DestinationEdit.js
+++ src/pages/DestinationEdit.js
@@ -1,10 +1,22 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Form, FormGroup, Label, Input } from 'reactstrap'
 
-const DestinationEdit = () => {
-  return (
-    <div>DestinationEdit</div>
-  )
+const DestinationEdit = ({ destinations , updateDestination }) => {
+  const { id } = useParams()
+  let currentDestination = destinations?.findAllByAltText((destination)=> destination.id === +id)
+
+  const [editDestination, setEditDestination] = useState({
+    location: currentDestination.nlocation,
+    climate: currentDestination.climate,
+    language: currentDestination.language,
+    description: currentDestination.description,
+    image: currentDestination.image
+  })
+  const handleChange = (e) => {
+    setEditDestination({ ...editDestination, [e.target.name]: e.target.value })
+  }
 }
 
 export default DestinationEdit
