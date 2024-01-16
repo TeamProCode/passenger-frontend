beaada8842e65121a327c2414af08234fe7165bd
anthonyholt <ant.scott227@gmail.com>
2024-01-16 13:39:51 -0800

    index and bio

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,95 +1,99 @@
-import React from 'react'
+import React from "react";
 import About from "../assests/About.svg";
 import backgroundPicture from "../assests/homeBg.png";
 
 const AboutUs = () => {
-
   const backgroundStyle = {
     backgroundImage: `url(${backgroundPicture})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    width: '100%',
-    height: '65vh',
-    position: 'relative',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "65vh",
+    position: "relative",
   };
 
   const newDestStyle = {
-    position: 'absolute',
-    textAlign: 'center',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    textAlign: "center",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
   };
 
   return (
     <div className="homeflex">
-        <div style={backgroundStyle}>
-          <div style={newDestStyle}>
-            <img
-            alt="About Us"
-            src={About}
-            width={480}
-            height={80}
-            />
-          </div>
+      <div style={backgroundStyle}>
+        <div style={newDestStyle}>
+          <img alt="About Us" src={About} width={480} height={80} />
+        </div>
       </div>
       <div className="bioflex">
-  <div className="biobox">
+        <div className="biobox">
+          <div className="title">Joseph Kong</div>
 
-      <div className="title">Joseph Kong</div>
+          <div className="parabio">
+            <p>
+              {" "}
+              Hi there! I am Joseph (Project Manager of Passenger), a life-long learner who has transitioned to
+              software engineering from physical therapy. I have traveled to
+              many places both in the U.S. and outside of the country. Fun fact,
+              my favorite city is San Diego!
+            </p>
+          </div>
+        </div>
 
-      <div className="parabio">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.    
-          </p>
+        <div className="imagebiokong"></div>
       </div>
-  </div>
 
-  <div className="imagebiokong"></div>
-</div>
+      <div className="bioflex">
+        <div className="biobox">
+          <div className="title">Andre Locsin</div>
 
-<div className="bioflex">
-  <div className="biobox">
+          <div className="parabio">
+            <p>
+              {" "}
+              I’ve traveled, and I like to travel. Time spent in idle is time wasted,
+              but time in idle is beauty. I’m André, junior software
+              developer & Product Manager of Passenger with a love and hunger
+              for exploration and tinkering.
+            </p>
+          </div>
+        </div>
 
-      <div className="title">Andre Locsin</div>
-
-      <div className="parabio">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          </p>
+        <div className="imagebioandre"></div>
       </div>
-  </div>
 
-  <div className="imagebioandre"></div>
-</div>
+      <div className="bioflex">
+        <div className="biobox">
+          <div className="title">Emery Winkeljohn</div>
 
-<div className="bioflex">
-  <div className="biobox">
+          <div className="parabio">
+            <p>
+              {" "}
+              Hey, Emery here! I serve as the Tech Lead for Passenger. I am an avid environmentalist and outdoors advocate. I spend most of my free time enjoying the Southern California landscape via snowboarding, mountain biking, surfing and hiking! I can't wait to see all of YOUR  outdoor adventures posted on Passenger!
+            </p>
+          </div>
+        </div>
 
-      <div className="title">Emery Winkeljohn</div>
-
-      <div className="parabio">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.   
-          </p>
+        <div className="imagebioem"></div>
       </div>
-  </div>
 
-  <div className="imagebioem"></div>
-</div>
+      <div className="bioflex">
+        <div className="biobox">
+          <div className="title">Anthony Holt</div>
 
-<div className="bioflex">
-  <div className="biobox">
+          <div className="parabio">
+            <p>
+              {" "}
+              Hi fellow Passengers!! My name is Anthony Holt and I serve as the Design Lead for the site. If I'm not being creative and coming up with new ideas, you can catch me enroute to the closest tropical setting in search of a good time and new things to be inspired by!
+            </p>
+          </div>
+        </div>
 
-      <div className="title">Anthony Holt</div>
-
-      <div className="parabio">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.    
-          </p>
+        <div className="imagebioant"></div>
       </div>
-  </div>
-
-  <div className="imagebioant"></div>
-</div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- src/pages/DestinationIndex.js
+++ src/pages/DestinationIndex.js
@@ -1,37 +1,75 @@
-import React from 'react';
+import React from "react";
 import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 import { NavLink } from "react-router-dom";
+import backgroundPicture from "../assests/homeBg.png";
+import Destinations from "../assests/Destinations.svg";
 
 const DestinationIndex = ({ destinations }) => {
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundPicture})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "65vh",
+    position: "relative",
+  };
+
+  const newDestStyle = {
+    position: "absolute",
+    textAlign: "center",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
+
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-      {destinations.map((destination, index) => (
-        <Card
-          style={{
-            width: "14rem",
-            margin: "10px", 
-          }}
-          key={index}
-        >
-          <img alt={`profile of a destination ${destination.location}`} src={destination.image} />
-          <CardBody>
-            <CardTitle tag="h5">{destination.location}</CardTitle>
-            <CardSubtitle className="" tag="h6">
-              Climate: {destination.climate}
-            </CardSubtitle>
+    <div className="homeflex">
+      <div style={backgroundStyle}>
+        <div style={newDestStyle}>
+          <img alt="Destinations" src={Destinations} width={480} height={80} />
+        </div>
+      </div>
 
-            <CardSubtitle>
-              Language: {destination.language}
-            </CardSubtitle>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {destinations.map((destination, index) => (
+          <Card
+            style={{
+              width: "14rem",
+              margin: "40px",
+              backgroundColor: "rgba(215, 188, 152, 0.6)",
+            }}
+            key={index}
+          >
+            <img
+              alt={`profile of a destination ${destination.location}`}
+              src={destination.image}
+              style={{ width: "100%", height: "150px" }}
+            />
+            <CardBody style={{ fontFamily: "Manuale, serif" }}>
+              <CardTitle tag="h5">{destination.location}</CardTitle>
+              <CardSubtitle className="" tag="h6">
+                Climate: {destination.climate}
+              </CardSubtitle>
 
-            <CardSubtitle>
-              Description: {destination.description}
-            </CardSubtitle>
-            
-            <NavLink to={`/destinationshow/${destination.id}`}>See More Details</NavLink>
-          </CardBody>
-        </Card>
-      ))}
+              <CardSubtitle>Language: {destination.language}</CardSubtitle>
+
+              <CardSubtitle>
+                Description: {destination.description}
+              </CardSubtitle>
+
+              <NavLink to={`/destinationshow/${destination.id}`}>
+                See More Details
+              </NavLink>
+            </CardBody>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
