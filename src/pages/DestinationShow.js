a6ac65d3c8a3f43ff8773f4e52fe9af90e93e3fd
anthonyholt <ant.scott227@gmail.com>
2024-01-17 09:13:33 -0800

    show style

--- src/pages/DestinationShow.js
+++ src/pages/DestinationShow.js
@@ -1,8 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { useParams, NavLink } from "react-router-dom"
 import { Button } from 'reactstrap'
-
-
 
 
 const DestinationShow = ({ destinations, photos, deleteDestination, }) => {
@@ -24,55 +22,56 @@ const DestinationShow = ({ destinations, photos, deleteDestination, }) => {
   // console.log((currentPhotos))
   console.log((destinations))
   console.log((currentPhotos))
-  return (
-    <main className="">
-      {currentDestination && (
-        <>
-          <img
-            alt={`profile of a Destination named ${currentDestination.location}`}
-            src={currentDestination.image}
-            className="Destination-show-img"
-            role="main-destination-image"
-            style={{ width: "300px", height: "auto" }}
-          />
-          <p>Location: {currentDestination.location}</p>
-          <p>Climate: {currentDestination.climate}</p>
-          <p>Language: {currentDestination.language}</p>
-          <p>Description: {currentDestination.description}</p>
-          <NavLink to={`/destinationedit/${currentDestination.id}`} className="nav-link">
+
+return (
+<main className="show-flex">
+  {currentDestination && (
+    <>
+      <img
+        alt={`profile of a Destination named ${currentDestination.location}`}
+        src={currentDestination.image}
+        className="Destination-show-img"
+        role="main-destination-image"
+        style={{ width: "600px", height: "auto", marginTop: '80px', marginBottom: '20px' }}
+      />
+      <div className="border-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+        <p>Location: {currentDestination.location}</p>
+        <p>Climate: {currentDestination.climate}</p>
+        <p>Language: {currentDestination.language}</p>
+        <p>Description: {currentDestination.description}</p>
+        <NavLink to={`/destinationedit/${currentDestination.id}`} className="nav-link">
+          <Button style={{ backgroundColor: '#B6706E', marginBottom: '10px' }}>
             Edit Destination
-          </NavLink>
-          <NavLink to={'/destinationindex'} className="nav-link">
-            <Button onClick={handleDelete}>
-              Remove This Destination
-            </Button>
-          </NavLink>
+          </Button>
+        </NavLink>
+        <NavLink to={'/destinationindex'} className="nav-link">
+          <Button onClick={handleDelete} style={{ backgroundColor: '#195789' }} >
+            Remove This Destination
+          </Button>
+        </NavLink>
+      </div>
 
-
-          {displayPhotos(currentPhotos).map((photoObject, index
-          ) => {
-            return (
-              <div key={index}> <img role="photo-data" src={photoObject.image} alt={photoObject.alt} />
-                <p>{photoObject.description}</p>
-                <NavLink to={`photoedit/${photoObject.id}`}
-                  className="nav-link">
-                  <button>
-                    Edit Photo
-                  </button>
-                </NavLink>
-              </div>
-            )
-          })}
-          <NavLink to={`/destinationshow/${id}/photonew`}>
-            <button>Add Photos </button>
-          </NavLink>
-        </>
-      )
-      }
-
-    </main >
-  )
+      {displayPhotos(currentPhotos).map((photoObject, index) => {
+        return (
+          <div key={index} className="text-center">
+            <img role="photo-data" src={photoObject.image} alt={photoObject.alt} style={{ width: "600px", height: "auto", marginTop: '80px' }} />
+            <p>{photoObject.description}</p>
+            <NavLink to={`photoedit/${photoObject.id}`} className="nav-link">
+              <Button style={{ backgroundColor: '#B6706E', marginBottom: '10px' }}>
+                Edit Photo
+              </Button>
+            </NavLink>
+          </div>
+        );
+      })}
+      <NavLink to={`/destinationshow/${id}/photonew`}>
+        <Button style={{ backgroundColor: '#195789' }} > Add Photos </Button>
+      </NavLink>
+    </>
+  )}
+</main>
+ )
 }
 
-
 export default DestinationShow
+
