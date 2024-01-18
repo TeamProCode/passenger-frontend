a768a91fe8825f4bcf6a6fe16c1f9c90d9239741
anthonyholt <ant.scott227@gmail.com>
2024-01-17 17:57:28 -0800

    more style fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,75 @@
-import React from 'react'
-import { Nav, NavItem } from 'reactstrap'
-import { NavLink, useNavigate } from 'react-router-dom';
-import Pass from '../assests/Pass.svg'
+import React from "react";
+import { Nav, NavItem } from "reactstrap";
+import { NavLink, useNavigate } from "react-router-dom";
+import Pass from "../assests/Pass.svg";
 
 const Header = ({ logout }) => {
-    const navigate = useNavigate()
-    const handleClick = () => {
-        logout()
-        navigate("/")
-      }
-    return (
-       <>
-        <img
+  const navigate = useNavigate();
+  const handleClick = () => {
+    logout();
+    navigate("/");
+  };
+  return (
+    <>
+      <img
         alt="logo"
         src={Pass}
         width={"480"}
         height={"80"}
-        style={{ marginLeft: '0 auto', display: 'block' }}
-        />
-        <header className='header'>
-            <Nav className='navi-bar'
-                justified
-                pills
-            ><NavItem >
-                    <NavLink
-                        to="/"
-                        className='navi-font'>
-                          Home   
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink to="/destinationindex"
-                        className='navi-font'>
-                          Take Me to Destinations  
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink to="/destinationnew"
-                        className='navi-font'>
-                         Add to Destinations 
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink to="/aboutus"
-                        className='navi-font'>
-                         About Us 
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink to="/signin"
-                        className='navi-font'>
-                         Sign-in/Sign-up
-                    </NavLink>
-                </NavItem>
-                <NavItem >
-                    <NavLink 
-                    className='navi-font'>
-                        <button onClick={handleClick}>
-                         Log Out
-                         </button> 
-                    </NavLink>
-                </NavItem>
+        style={{ marginLeft: "0 auto", display: "block" }}
+      />
+      <header className="header">
+        <Nav className="navi-bar" justified pills>
+          <NavItem>
+            <NavLink to="/" className="navi-font">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/destinationindex" className="navi-font">
+              Take Me to Destinations
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/destinationnew" className="navi-font">
+              Add to Destinations
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/aboutus" className="navi-font">
+              About Us
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/signin" className="navi-font">
+              Sign-in/Sign-up
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="navi-font">
+              <Button
+                type="submit"
+                value="login"
+                style={{
+                  backgroundColor: "#195789",
+                  marginBottom: "10px",
+                  textDecoration: "none",
+                  display: "inline-block",
+                  padding: "10px 20px",
+                  color: "#C3A78B",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+                onClick={handleClick}
+              >
+                Log Out
+              </Button>
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </header>
+    </>
+  );
+};
 
-            </Nav >
-        </header>
-        </> 
-    )
-}
-
-export default Header
+export default Header;
